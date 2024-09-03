4304ffd0735741030da68871edf116142a88ad28
Sir Vin <109795015+vinn65@users.noreply.github.com>
2024-09-03 17:16:32 +0300

    Update settings.py

--- readr/settings.py
+++ readr/settings.py
@@ -15,6 +15,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 
 # Application definition
 
